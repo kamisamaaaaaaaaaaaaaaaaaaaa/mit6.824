db841cd2df5cf00eb2097589643ee7e355d2a91a
lys <1136433970@qq.com>
2023-03-26 15:29:46 +0800

    lab1-labreplicate-binary-version

--- src/raft/config.go
+++ src/raft/config.go
@@ -563,7 +563,7 @@ func (cfg *config) wait(index int, n int, startTerm int) interface{} {
 func (cfg *config) one(cmd interface{}, expectedServers int, retry bool) int {
 	t0 := time.Now()
 	starts := 0
-	for time.Since(t0).Seconds() < 20 && cfg.checkFinished() == false {
+	for time.Since(t0).Seconds() < 10 && cfg.checkFinished() == false {
 		// try all the servers, maybe one is the leader.
 		index := -1
 		for si := 0; si < cfg.n; si++ {
@@ -587,9 +587,9 @@ func (cfg *config) one(cmd interface{}, expectedServers int, retry bool) int {
 			// somebody claimed to be the leader and to have
 			// submitted our command; wait a while for agreement.
 			t1 := time.Now()
-			for time.Since(t1).Seconds() < 4 {
+			for time.Since(t1).Seconds() < 2 {
 				nd, cmd1 := cfg.nCommitted(index)
-				// fmt.Printf("index =%v ,nd = %v,cmd=%v ,cmd1 = %v\n", index, nd, cmd, cmd1)
+				// fmt.Printf("index =%v ,nd = %v,expectservers = %v, cmd=%v ,cmd1 = %v\n", index, nd, expectedServers, cmd, cmd1)
 				if nd > 0 && nd >= expectedServers {
 					// committed
 					if cmd1 == cmd {
